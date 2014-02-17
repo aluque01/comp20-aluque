55ca51772f90aac83c8d74c16c537dad7619fcf5
Ariel Luque <ariel.luque@tufts.edu>
2014-02-17 17:05:31 -0500

    added readme, finally got canvas to display a picture

---
+++ duckhunt/README.txt
@@ -0,0 +1,6 @@
+READ ME
+Duckhunt
+
+So far the only thing implemented has been the canvas get method. The image finally draws after using the onload event. 
+
+So far I'm trying to get the images to place. 
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,15 @@
 // Your work goes here...
+//assets/duckhunt.png
+//ctx.drawImage(img,0,0);
+function draw(){
+	var canvas = document.getElementById('game'); 
+	ctx = canvas.getContext('2d'); 
+
+	var img = new Image();
+  	img.onload = function(){
+  		ctx.drawImage(img,0,0);
+  	};
+	img.src = 'assets/duckhunt.png';
+}
+
+
