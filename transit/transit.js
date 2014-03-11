da912d90dd14796a2e9868e3161e2e65a6c60f5d
Ariel Luque <ariel.luque@tufts.edu>
2014-03-11 19:45:31 -0400

    not working

--- transit/transit.js
+++ transit/transit.js
@@ -66,8 +66,7 @@ function findClosestStation(line){
 	console.log(min);  
 	console.log(index); 
 
-	var headsUp = document.getElementById("invisible"); 
-	headsUp.innerHTML = "<p>Closest station: "+ line[index]["Station"] + "</br> Distance: " + min + " miles. </p>";
+	document.getElementById("invisible").innerHTML = "<p>Closest station: "+ line[index]["Station"] + "</br> Distance: " + min + " miles. </p>";
 
 
 
