57aa17b5267151afc93e02378e440c324ddfdb19
Ariel Luque <ariel.luque@tufts.edu>
2014-03-10 20:37:53 -0400

    testing gps

--- transit/transit.js
+++ transit/transit.js
@@ -1,9 +1,33 @@
+var Lat = 0; 
+var Lng = 0;
 
-function initialize(){
+
+
+function start(){
+	var location = new google.maps.LatLng(Lat, Lng);
+
 	var mapOptions = {
-		 center: new google.maps.LatLng(0, 0),
-		 zoom: 2
+	 center: location,
+	 zoom: 2
 	}; 
+
 	var map = new google.maps.Map(document.getElementById("map-canvas"), 
 		mapOptions)
+
+	locate(); 
+
+}
+
+
+function locate(){
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position){
+			Lat = position.coords.latitude; 
+			Lng = position.coords.longitude; 
+			console.log(Lat); 
+			console.log(Lng); 
+		});
+	} else {
+		alert("Looks like the NSA can't find you."); 
+	}
 }
